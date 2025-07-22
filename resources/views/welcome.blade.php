72d8a1046c53485427e2dbb201b68c9de051a2bb
Htet Ko Oo <htetkooo2532@gmail.com>
2025-07-22 12:30:53 +0700

    editing spelling and adding two p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,9 @@
     <title>Laravel Cloud</title>
 </head>
 <body>
-    <h1>Hello Larave Cloud</h1>
+    <h1>Hello Laravel Cloud</h1>
     <p>I am testing to push laravel project to laravel cloud.</p>
+    <p>Nice to meet you.</p>
+    <p>see you again.</p>
 </body>
 </html>
